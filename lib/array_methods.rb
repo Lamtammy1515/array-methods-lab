6c8967828aa807e8cea281bd7fb79fdc54ccb4d0
Tammy Lam <lamtammy1515@gmail.com>
2019-12-10 23:07:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,24 +1,23 @@
 def using_include(array, element)
-junk_food = ["pizza", "oreos", "burgers", "fries"]
-junk_food.include? ("pasta")
+array.include?(element)
 end
 
 def using_sort(array)
-junk_food.sort
+array.sort
 end
 
 def using_reverse(array)
-junk_food.reverse
+array.reverse
 end
 
 def using_first(array)
-junk_food.first
+array.first
 end
 
 def using_last(array)
-junk_food.last
+array.last
 end
 
 def using_size(array)
-junk_food.size
+array.size
 end
