42671c43a119bc58ef60dfd05ad3a3a341ed1633
Tammy Lam <lamtammy1515@gmail.com>
2019-12-10 23:04:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,21 +4,21 @@ junk_food.include? ("pasta")
 end
 
 def using_sort(array)
-
+junk_food.sort
 end
 
 def using_reverse(array)
-
+junk_food.reverse
 end
 
 def using_first(array)
-
+junk_food.first
 end
 
 def using_last(array)
-
+junk_food.last
 end
 
 def using_size(array)
-
+junk_food.size
 end
